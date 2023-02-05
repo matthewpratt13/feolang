b87228e3de359539928d5a449525aa656dfdb4a8
Matthew Pratt <matthew.pratt13@gmail.com>
2023-02-05 20:35:32 +0200

    refactor file structure and add tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,25 @@
 pub mod error;
+
+#[cfg(test)]
+mod tests {
+    use std::path::Path;
+
+    #[test]
+    fn it_reads_a_file() {
+        let path = Path::new("./examples/ex01.feo");
+        match std::fs::read_to_string(path) {
+            Ok(f) => f,
+            Err(e) => match e.kind() {
+                std::io::ErrorKind::NotFound => panic!("Unable to locate file"),
+                std::io::ErrorKind::InvalidInput => panic!("Path not valid ({:?})", path),
+                _ => panic!("Unable to read file"),
+            },
+        };
+    }
+
+    #[test]
+    fn it_checks_file_contents() {
+        let path = Path::new("./examples/ex01.feo");
+        assert!(!std::fs::read_to_string(path).unwrap().is_empty());
+    }
+}
